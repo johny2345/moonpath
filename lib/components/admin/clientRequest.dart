ed0247cdd72b688fc4938859dd5984f95740654c
Nicho <nicholjohntejada@yahoo.com>
2021-10-08 04:47:00 +0800

    client details page

--- lib/components/admin/clientRequest.dart
+++ lib/components/admin/clientRequest.dart
@@ -36,21 +36,66 @@ class _ClientRequestPageState extends State<ClientRequestPage> {
         } else if (snapshot.connectionState == ConnectionState.waiting) {
           return WidgetProperties().loadingProgress(context);
         }
-        return ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
-            print(data);
-            return ListTile(
+        return _displayRequestList(context, snapshot);
+      },
+    );
+  }
+
+  _displayRequestList(BuildContext context, AsyncSnapshot snapshot) {
+    String? month;
+
+    return ListView(
+      children: snapshot.data!.docs.map<Widget>((document) {
+        DateTime? requestDate = document['schedule'].toDate();
+        if (requestDate!.month == 1) {
+          month = 'January';
+        } else if (requestDate.month == 2) {
+          month = 'February';
+        } else if (requestDate.month == 3) {
+          month = 'March';
+        } else if (requestDate.month == 4) {
+          month = 'April';
+        } else if (requestDate.month == 5) {
+          month = 'May';
+        } else if (requestDate.month == 6) {
+          month = 'June';
+        } else if (requestDate.month == 7) {
+          month = 'July';
+        } else if (requestDate.month == 8) {
+          month = 'August';
+        } else if (requestDate.month == 9) {
+          month = 'September';
+        } else if (requestDate.month == 10) {
+          month = 'October';
+        } else if (requestDate.month == 11) {
+          month = 'November';
+        } else if (requestDate.month == 12) {
+          month = 'December';
+        }
+        Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+        print(data);
+        return Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(3.0)),
+            child: ListTile(
+              leading: Icon(Icons.request_page_rounded),
               title: Text(data['name'].toString()),
-              subtitle: Text(data['schedule'].toString()),
+              subtitle: Text(month.toString() +
+                  ' ' +
+                  requestDate.day.toString() +
+                  ',' +
+                  ' ' +
+                  requestDate.year.toString()),
               onTap: () {
                 print(data['description']);
               },
-            );
-          }).toList(),
+            ),
+          ),
         );
-      },
+      }).toList(),
     );
   }
 }
