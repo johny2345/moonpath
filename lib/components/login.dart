fdc7c099432b223c5ed12085356c6ae6e723dbfd
= <=>
2022-12-07 05:31:52 +0800

    remove user

--- lib/components/login.dart
+++ lib/components/login.dart
@@ -162,7 +162,7 @@ class _LoginPageState extends State<LoginPage> {
           MaterialPageRoute(builder: (context) => AdminHomePage()),
           (Route<dynamic> route) => false,
         );
-        print(user);
+        print
       } on FirebaseAuthException catch (e) {
         print('-------------THE USER HAS NOT LOGGED IN-----------------------');
         print(e.code);
