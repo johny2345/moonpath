96d97382f08bb6cfa6c22084fc346bc1dd602c32
= <nicholjohntejada@yahoo.com>
2022-12-11 02:02:15 +0800

    remove imports

--- lib/api/apiProvider.dart
+++ lib/api/apiProvider.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebasese_core.dart';
 import 'package:moonpath/widgets/widgetProperties.dart';
 
 FirebaseFirestore firestore = FirebaseFirestore.instance;
