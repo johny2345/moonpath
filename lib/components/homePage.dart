66e485e0e93de4d376ec29293e4c6f1a0c0ee793
= <nicholjohntejada@yahoo.com>
2022-12-18 13:41:58 +0800

    remoadsf

--- lib/components/homePage.dart
+++ lib/components/homePage.dart
@@ -83,9 +83,7 @@ class _HomepageState extends State<Homepage> {
       print('-----CONNECTED MOBLE DATA-------');
     } else if (connectivityResult == ConnectivityResult.wifi) {
       print('----------CONNECTED WIFI NETWORK--------');
-    } else {
-      print('---NOT CONNECTED----');
-    }
+    } else {}
   }
 
   @override
