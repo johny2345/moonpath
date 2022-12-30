4085424f84ef332ef5eb48c466858f601bc49b03
= <nicholjohntejada@yahoo.com>
2022-12-31 00:38:14 +0800

    adf

--- lib/components/bookClient.dart
+++ lib/components/bookClient.dart
@@ -430,7 +430,7 @@ class _BookPageState extends State<BookPage> {
         today.second.toString() +
         today.millisecond.toString();
 
-    // print('----------------------
+    // print('----------
     // print('get data: $_date $_timeController $_time');
     // print('----------------->$timeFormat $paymentMethod');
 
